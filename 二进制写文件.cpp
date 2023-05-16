6297667b92f595f7a53a91fcaaadfdb6f0acdecb
lv_hangyu <sakura316694@163.com>
2023-05-16 11:01:47 +0800

    5.16 二进制读写

---
+++ 二进制写文件.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//#include<fstream>
+//#include<string>
+//class Person {
+//public:
+//	char m_Name[64];
+//	int m_Age;
+//};
+//void test01() {
+//	ofstream ofs("person.txt", ios::out | ios::binary);
+//	/*ofs.open("perosn.txt",ios::out|ios::binary);*/
+//	Person p = { "уехЩ",18 };
+//	ofs.write((const char*)&p, sizeof(p));
+//	ofs.close();
+//}
+//int main() {
+//	test01();
+//	system("pause");
+//	return 0;
+//}
